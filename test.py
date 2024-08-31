049b234a6b6a4f49768b71bf16dfccf195345225
Pluxxa <95534978+Pluxxa@users.noreply.github.com>
2024-08-31 18:18:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ def test_no_vowels():
 
 def test_mixed_string():
     assert count_vowels("Привет Мир") == 3
-    assert count_vowels("Тестирование") == 7
+    assert count_vowels("Тестирование") == 6
     assert count_vowels("ПРОГРАММИРОВАНИЕ") == 7
     assert count_vowels("12345!@#") == 0
